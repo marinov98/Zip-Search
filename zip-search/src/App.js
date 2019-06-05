adb47001e2daf48771a51b133b4bb797ae12c63b
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2019-06-05 12:21:09 -0400

    record value

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -2,13 +2,30 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      zipcode: ""
+    };
+  }
+
+  getZip = e => {
+    this.setState({ zipcode: e.target.value });
+  };
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h1>Zip Code Search</h1>
           <span> Zipe Code: </span>
-          <input type="text" name="zipcode" />
+          <input
+            onChange={this.getZip}
+            value={this.state.zipcode}
+            type="text"
+            name="zipcode"
+          />
         </div>
       </div>
     );
