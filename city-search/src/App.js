6b4004ba78d6d74586d8fc1a95fef4698a1b173c
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2019-06-05 21:02:04 -0400

    tasks finished

--- city-search/src/App.js
+++ city-search/src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cityName: "",
+      data: []
+    };
+  }
+
+  getCityName = e => {
+    this.setState({ cityName: e.target.value });
+  };
+
+  fetchZipCodes = async () => {
+    try {
+      let { data } = await axios.get(
+        "http://ctp-zip-api.herokuapp.com/city/" + this.state.cityName
+      );
+      this.setState({ data: data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  render() {
+    let zips = this.state.data.map(zip => <li key={zip}>{zip}</li>);
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>City Search</h1>
+          <span> City: </span>
+          <input
+            onChange={this.getCityName}
+            value={this.state.cityName}
+            type="text"
+            name="cityName"
+          />
+          <button onClick={this.fetchZipCodes}>Search</button>
+          <ul>{zips}</ul>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
